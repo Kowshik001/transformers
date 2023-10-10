8bca9d68f5c57a59235c6cf5d61eb2b375646fc9
BAKI LEELA KOWSHIK REDDY <108167224+Kowshik001@users.noreply.github.com>
2023-10-10 23:23:20 +0530

    Create Inorder, Preorder, PostOrder.py

---
+++ Traversal techniques/Inorder, Preorder, PostOrder.py
@@ -0,0 +1,48 @@
+class TreeNode:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return TreeNode(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+def preorder_traversal(root):
+    if root:
+        print(root.val, end=' ')
+        preorder_traversal(root.left)
+        preorder_traversal(root.right)
+
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.val, end=' ')
+        inorder_traversal(root.right)
+
+def postorder_traversal(root):
+    if root:
+        postorder_traversal(root.left)
+        postorder_traversal(root.right)
+        print(root.val, end=' ')
+
+root = None
+values = [50, 30, 70, 20, 40, 60, 80]
+
+for value in values:
+    root = insert(root, value)
+
+print("Preorder Traversal:")
+preorder_traversal(root)
+
+print("\nInorder Traversal:")
+inorder_traversal(root)
+
+print("\nPostorder Traversal:")
+postorder_traversal(root)
